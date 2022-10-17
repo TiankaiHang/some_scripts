961ea6dafb3e2bee435beff7ace27a8841c9b36d
Tiankai Hang <seuhtk98@gmail.com>
2022-10-17 15:40:44 +0800

    Create make_big_pic.py

---
+++ viz/make_big_pic.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+import glob
+
+
+def main():
+    data_dir = "D:/LOGS/2022-10/val_1w"
+    target_path = "tmp.png"
+    
+    M = 9
+    N = 16
+    resolution = 64
+
+    total_files = glob.glob(f"{data_dir}/*.jpg")
+    total_files = total_files[:(M * N)]
+
+    target_img = np.zeros((M * resolution, N * resolution, 3), dtype=np.uint8)
+
+    for i, _f in enumerate(total_files):
+        img = cv2.imread(_f)
+        target_img[i//N * resolution:(i//N + 1) * resolution, i%N * resolution:(i%N + 1) * resolution] = img
+
+    cv2.imwrite(filename=target_path, img=target_img)
+
+
+if __name__ == '__main__':
+    main()
